7f9cf6c09496d08ce23d1c881609da289ed46eca
bizarf <snesmaster40@googlemail.com>
2023-09-19 23:07:14 +0100

    rewrote the router coding to use createHashRouter instead of Router components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import { useState } from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import User from "./types/user";
-import Homepage from "./components/Homepage";
-import Login from "./components/Login";
-import Post from "./components/Post";
-import SignUp from "./components/SignUp";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import Cookies from "universal-cookie";
 import jwtDecode from "jwt-decode";
 import JwtDecodeType from "./types/jwt_decode";
+import Router from "./Router";
 
 const App = () => {
     const [theme, setTheme] = useState<string>();
@@ -42,39 +36,13 @@ const App = () => {
     return (
         // set the app container to a minimum height 100% to allow the div to take up the rest of the space. a must have for the dark mode background colour
         <div className="flex min-h-full flex-col dark:bg-slate-600">
-            <HashRouter>
-                {/* flex to make a sticky footer */}
-                <div className="flex-[1_0_auto]">
-                    <Header
-                        user={user}
-                        setUser={setUser}
-                        theme={theme}
-                        setTheme={setTheme}
-                        fetchUserData={fetchUserData}
-                    />
-                    <Routes>
-                        <Route path="/" element={<Homepage />} />
-                        <Route
-                            path="/login"
-                            element={
-                                <Login
-                                    user={user}
-                                    fetchUserData={fetchUserData}
-                                />
-                            }
-                        />
-                        <Route
-                            path="/sign-up"
-                            element={<SignUp user={user} />}
-                        />
-                        <Route
-                            path="/posts/:id"
-                            element={<Post user={user} />}
-                        />
-                    </Routes>
-                </div>
-                <Footer theme={theme} />
-            </HashRouter>
+            <Router
+                user={user}
+                setUser={setUser}
+                theme={theme}
+                setTheme={setTheme}
+                fetchUserData={fetchUserData}
+            />
         </div>
     );
 };

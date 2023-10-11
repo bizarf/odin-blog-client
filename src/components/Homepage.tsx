1c10e076917d54f6785060f58748e34906b4eebb
bizarf <snesmaster40@googlemail.com>
2023-10-11 22:09:42 +0100

    added react paginate package. added pagination feature to allow loading more pages.

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,35 +1,54 @@
 import { useState, useEffect } from "react";
 import dayjs from "dayjs";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import PostsType from "../types/posts";
+import ReactPaginate from "react-paginate";
 
 const Homepage = () => {
     const [posts, setPosts] = useState<PostsType[]>([]);
-    // const [totalPosts, setTotalPosts] = useState<number>();
+    const [totalPosts, setTotalPosts] = useState<number>();
+
+    // get the current page number from the search parameters
+    const [searchParams, setSearchParams] = useSearchParams();
+    const currentPageNumber = Number(searchParams.get("page"));
 
     const fetchPosts = () => {
-        fetch("https://odin-blog-api-ofv2.onrender.com/api/posts")
+        fetch(
+            `https://odin-blog-api-ofv2.onrender.com/api/posts?page=${currentPageNumber}`
+        )
             .then((response) => {
                 return response.json();
             })
             .then((data) => {
                 if (data.success) {
                     setPosts(data.allPosts);
-                    // setTotalPosts(data.totalPublishedPostsCount);
+                    setTotalPosts(data.totalPublishedPostsCount);
                 }
             });
     };
 
     useEffect(() => {
         fetchPosts();
-    }, []);
+    }, [currentPageNumber]);
+
+    const totalPages = Math.ceil(totalPosts! / 10);
+
+    // pagination functions
+    const handlePageClick = (e: { selected: number }) => {
+        // e.selected returns values starting from 0. I'll need to add 1 to this or else the wrong pages will be returned.
+        const page = e.selected + 1;
+        // searchParams set takes in a name and a value. e.selected returns a number, so need to convert that to string to use in search params
+        searchParams.set("page", page.toString());
+        // once that's done update the search params
+        setSearchParams(searchParams);
+    };
 
     return (
         <div className="">
             <h1 className="py-4 text-center text-4xl font-bold italic text-gray-800 dark:text-white">
                 Welcome to the blog
             </h1>
-            <div>
+            <div className="flex flex-col">
                 <h2 className="py-4 text-center text-3xl font-bold text-gray-800 dark:text-white">
                     Articles
                 </h2>
@@ -61,6 +80,24 @@ const Homepage = () => {
                             );
                         })}
                 </div>
+                {totalPages && (
+                    <ReactPaginate
+                        pageCount={totalPages}
+                        breakLabel="..."
+                        nextLabel="Next >"
+                        pageRangeDisplayed={5}
+                        previousLabel="< Previous"
+                        renderOnZeroPageCount={null}
+                        onPageChange={handlePageClick}
+                        className="flex items-center justify-center space-x-4 py-2"
+                        activeClassName="bg-blue-500 justify-center"
+                        pageClassName="w-10 h-10 hover:text-blue-600 p-4 inline-flex items-center text-sm font-medium rounded-full dark:text-white"
+                        previousClassName="dark:text-white hover:text-blue-600 p-4 inline-flex items-center gap-2 rounded-md"
+                        nextClassName="dark:text-white hover:text-blue-600 p-4 inline-flex items-center gap-2 rounded-md"
+                        disabledClassName="pointer-events-none"
+                        breakClassName="dark:text-white"
+                    />
+                )}
             </div>
         </div>
     );

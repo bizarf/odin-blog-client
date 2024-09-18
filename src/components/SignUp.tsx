ef12eaeddbe5f351ccb9a81190130f9bc5f3fa4e
Tony Hoong <tonyhoong40@gmail.com>
2024-09-18 20:11:13 +0100

    remade the sign up and login forms to use shadcn ui form components

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ErrorsType from "../types/errors";
 import useUserStore from "../stores/useUserStore";
@@ -17,11 +17,6 @@ import {
 import { Input } from "@/components/ui/input";
 
 const SignUp = () => {
-    // const [firstname, setFirstname] = useState<string>();
-    // const [lastname, setLastname] = useState<string>();
-    // const [username, setUsername] = useState<string>();
-    // const [password, setPassword] = useState<string>();
-    // const [confirmPassword, setConfirmPassword] = useState<string>();
     const [error, setError] = useState<[ErrorsType] | []>([]);
     // if the success state is true, then the form will disappear and a success message will be displayed to the user
     const [success, setSuccess] = useState<boolean>(false);
@@ -32,9 +27,23 @@ const SignUp = () => {
     const { user } = useUserStore();
 
     const signUpFormSchema = z.object({
-        firstname: z.string().min(1, { message: "First name is required" }),
-        lastname: z.string().min(1, { message: "Last name is required" }),
-        username: z.string().min(1, { message: "Username is required" }),
+        firstname: z
+            .string()
+            .min(2, {
+                message: "First name must be at least 2 characters long",
+            })
+            .regex(/^[a-zA-Z]+$/, {
+                message: "First name can only contain letters",
+            }),
+        lastname: z
+            .string()
+            .min(2, { message: "Last name must be at least 2 characters long" })
+            .regex(/^[a-zA-Z]+$/, {
+                message: "Last name can only contain letters",
+            }),
+        username: z
+            .string()
+            .email({ message: "Username must be a valid email address" }),
         password: z
             .string()
             .min(8, { message: "Password must be at least 8 characters long" }),
@@ -54,13 +63,15 @@ const SignUp = () => {
         },
     });
 
-    // const sendSignUp = (e: React.FormEvent<HTMLFormElement>) => {
     const sendSignUp = async (values: z.infer<typeof signUpFormSchema>) => {
-        console.log(values);
+        // clear the errors if there is one already there
+        setError([]);
+
+        // if the password and confirm password do not match, then return an error
         if (values.password !== values.confirmPassword) {
             form.resetField("password");
             form.resetField("confirmPassword");
-            return setError([
+            setError([
                 {
                     location: "",
                     path: "",
@@ -69,40 +80,38 @@ const SignUp = () => {
                     msg: "Passwords do not match",
                 },
             ]);
+            return setTimeout(() => {
+                setError([]);
+            }, 5000);
         }
-        // e.preventDefault();
-        // make an object with the sign up detail states
-        // const data = {
-        //     firstname,
-        //     lastname,
-        //     username,
-        //     password,
-        //     confirmPassword,
-        // };
-        // // start fetch api, with a post method and set the header content type to json
-        // fetch("https://odin-blog-api-ofv2.onrender.com/api/sign-up", {
-        //     method: "POST",
-        //     headers: {
-        //         "Content-Type": "application/json",
-        //     },
-        //     // need to stringify the username and password to be able to send them as JSON objects
-        //     body: JSON.stringify(data),
-        // })
-        //     .then((res) => res.json())
-        //     .then((data) => {
-        //         // data object can either return a token or errors. if we get the token object, then we decode the token and set that as the user state. we store the jwt in the cookie.
-        //         if (data.message === "Sign up was successful!") {
-        //             // some sort of way to show sign up successful message
-        //             setSuccess((state) => !state);
-        //             setTimeout(() => {
-        //                 navigate("/");
-        //             }, 5000);
-        //         } else {
-        //             // error messages go here
-        //             setError(data.errors);
-        //             console.log(error);
-        //         }
-        //     });
+
+        // start fetch api, with a post method and set the header content type to json
+        fetch("https://odin-blog-api-ofv2.onrender.com/api/sign-up", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            // need to stringify the values object to be able to send them as a JSON payload
+            body: JSON.stringify(values),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                // data object can either return a token or errors. if we get the token object, then we decode the token and set that as the user state. we store the jwt in the cookie.
+                if (data.message === "Sign up was successful!") {
+                    // some sort of way to show sign up successful message
+                    setSuccess((state) => !state);
+                    setTimeout(() => {
+                        navigate("/");
+                    }, 5000);
+                } else {
+                    // error messages go here
+                    setError(data.errors);
+                    console.log(error);
+                    return setTimeout(() => {
+                        setError([]);
+                    }, 5000);
+                }
+            });
     };
 
     // if user is already logged in, then send them back to the home page instead of letting them try to sign up again
@@ -135,6 +144,7 @@ const SignUp = () => {
                                                 placeholder="First Name"
                                                 {...field}
                                                 className="dark:bg-slate-900"
+                                                maxLength={32}
                                             />
                                         </FormControl>
                                         <FormMessage />
@@ -152,6 +162,7 @@ const SignUp = () => {
                                                 placeholder="Last Name"
                                                 {...field}
                                                 className="dark:bg-slate-900"
+                                                maxLength={32}
                                             />
                                         </FormControl>
                                         <FormMessage />
@@ -163,12 +174,14 @@ const SignUp = () => {
                                 name="username"
                                 render={({ field }) => (
                                     <FormItem>
-                                        <FormLabel>Username</FormLabel>
+                                        <FormLabel>Username (E-mail)</FormLabel>
                                         <FormControl>
                                             <Input
-                                                placeholder="Username"
+                                                placeholder="Username (E-mail)"
                                                 {...field}
                                                 className="dark:bg-slate-900"
+                                                maxLength={255}
+                                                type="email"
                                             />
                                         </FormControl>
                                         <FormMessage />
@@ -183,9 +196,11 @@ const SignUp = () => {
                                         <FormLabel>Password</FormLabel>
                                         <FormControl>
                                             <Input
+                                                type="password"
                                                 placeholder="Password"
                                                 {...field}
                                                 className="dark:bg-slate-900"
+                                                maxLength={32}
                                             />
                                         </FormControl>
                                         <FormMessage />
@@ -200,9 +215,11 @@ const SignUp = () => {
                                         <FormLabel>Confirm Password</FormLabel>
                                         <FormControl>
                                             <Input
+                                                type="password"
                                                 placeholder="Confirm Password"
                                                 {...field}
                                                 className="dark:bg-slate-900"
+                                                maxLength={32}
                                             />
                                         </FormControl>
                                         <FormMessage className="text-red-600" />
